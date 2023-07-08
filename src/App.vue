b185420857ae66591f4c94205bd6eed7f3d0f2a9
leonemunyao <leonemunyao@gmail.com>
2023-07-08 15:36:43 +0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -34,11 +34,29 @@
              const app = createApp(App)
          </li>
        </ul>
+       <br>
+        While many examples in this guide only need a single component,
+        most real applications are organized into a tree of nested, reusable components. 
+        For example, a Todo application's component tree might look like this:
+        <ul>
+          <li>
+            App (root component)
+   ├─ TodoList
+   │  └─ TodoItem
+   │     ├─ TodoDeleteButton
+   │     └─ TodoEditButton
+   └─TodoFooter
+     ├─ TodoClearButton
+     └─ TodoStatistics
+          </li>
+        </ul>
      </p>
     </div>
    </div>
 </template>
 
 <style scoped>
+
+  
 
 </style>
