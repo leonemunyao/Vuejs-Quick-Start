c447492d170fd1b462fb46633d752fd0a46bd0c3
leonemunyao <leonemunyao@gmail.com>
2023-07-08 14:43:52 +0300

    changes

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { reactive, ref } from 'vue'
+
+  const counter = reactive({ count: 4 })
+  const message = ref('I am Leon')
+</script>
+
+<template>
+   <h1>{{ message }}</h1>
+   <p>The count is {{ counter.count }}</p>
+</template>
+
+<style scoped>
+
+</style>
